699fbb257a839d91040eb65c9a483f2cd4fdbe06
seremwen <seremwe.81@gmail.com>
2020-07-07 17:50:40 +0200

    Add files via upload

---
+++ DAZ/admin/includes/sidebar.php
@@ -0,0 +1,19 @@
+<nav class="ts-sidebar">
+			<ul class="ts-sidebar-menu">
+			
+				<li class="ts-label">Main</li>
+				<li><a href="dashboard.php"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+					<li><a href="#"><i class="fa fa-files-o"></i> MESSAGES</a>
+					<ul>
+						<li><a href="manage-messages.php">VIEW MESSAGES</a></li>
+						
+					</ul>
+				</li>
+					
+				
+				
+			
+				
+
+			
+		</nav>
